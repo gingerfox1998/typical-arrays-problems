9576bda8b256c903276b33c5d9ae3e9155dd82a6
gingerfox1998 <60549675+gingerfox1998@users.noreply.github.com>
2020-03-22 02:10:34 +0400

    feat: fix empty array

--- src/index.js
+++ src/index.js
@@ -1,29 +1,44 @@
 
 exports.min = function min (array) {
   var minarr = array[0];
-    for(i = 1; i < array.length; i++){
-        if(array[i] < minarr) {
-            minarr = array[i];
+    if(array.length != 0) {
+        for(i = 1; i < array.length; i++){
+            if(array[i] < minarr) {
+                minarr = array[i];
+            }
         }
+    }
+    else {
+        minarr = 0;
     }
   return minarr;
 }
 
 exports.max = function max (array) {
   var maxarr = array[0];
-    for(i = 1; i < array.length; i++){
-        if(array[i] > maxarr) {
-            maxarr = array[i];
+    if(array.length != 0) {
+        for(i = 1; i < array.length; i++){
+            if(array[i] > maxarr) {
+                maxarr = array[i];
+            }
         }
+    }
+    else {
+        maxarr = 0;
     }
   return maxarr;
 }
 
 exports.avg = function avg (array) {
   var avgarr = 0;
-    for(i = 0; i < array.length; i++){
-        avgarr = avgarr + array[i];
+    if(array.length != 0) {
+        for(i = 0; i < array.length; i++){
+            avgarr = avgarr + array[i];
+        }
+        avgarr = avgarr / array.length;
     }
-    avgarr = avgarr / array.length;
+    else {
+        avgarr = 0;
+    }
   return avgarr;
 }
