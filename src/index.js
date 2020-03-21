0582156843d18c34b3ed1c74b9fed3c0d7e39e70
gingerfox1998 <60549675+gingerfox1998@users.noreply.github.com>
2020-03-22 01:37:36 +0400

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 
 exports.min = function min (array) {
-  return 0;
+  var minarr = array[0];
+    for(i = 1; i < array.length; i++){
+        if(array[i] < array[i - 1]) {
+            minarr = array[i];
+        }
+    }
+  return minarr;
 }
 
 exports.max = function max (array) {
-  return 0;
+  var maxarr = array[0];
+    for(i = 1; i < array.length; i++){
+        if(array[i] > array[i - 1]) {
+            maxarr = array[i];
+        }
+    }
+  return maxarr;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  var avgarr = 0;
+    for(i = 0; i < array.length; i++){
+        avgarr = avgarr + array[i];
+    }
+    avgarr = avgarr / (array.length - 1);
+  return avgarr;
 }
